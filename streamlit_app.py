a6cc548ca69a748a6905197125566bfe4fad90c2
gobindaKD <32408582+gobindaKD@users.noreply.github.com>
2025-11-28 18:45:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,9 +50,10 @@ st.subheader("Underlying data")
 st.dataframe(queried_data, use_container_width=True)
 
 
-my_dataframe = session.table("smoothies.public.fruit_options")
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+                                                                      
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
