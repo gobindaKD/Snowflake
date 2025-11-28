80f04b39e30f89dec03e3e5dbe58b98d3956082f
gobindaKD <32408582+gobindaKD@users.noreply.github.com>
2025-11-28 18:32:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: {st.__version__}")
@@ -61,10 +63,13 @@ if ingredients_list:
   st.text(ingredients_list)
     
   ingredients_string = ''
-
+    
   for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen
+    ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=true)
 
+  
   st.write(ingredients_string)
 
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
@@ -77,7 +82,4 @@ if ingredients_list:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=true)
+
